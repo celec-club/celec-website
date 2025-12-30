5f6cd1fdf3914b8b5a926137ee39dfde7dff4f76
Yasser <yasserfedsi34@gmail.com>
2025-12-30 02:07:19 +0100

    feat: Update sponsors list and add technology sponsor

--- src/utils/sponsors.ts
+++ src/utils/sponsors.ts
@@ -3,21 +3,27 @@ interface SProps {
   logo: string;
 }
 
+export const technologySponsor: SProps[] = [
+  {
+    name: "Algerie Telecom",
+    logo: "/sponsors/at.png",
+  },
+];
+
 export const sponsors: SProps[] = [
-  { name: "Algerie Telecom", logo: "/sponsors/at.svg" },
+  { name: "Algerie Telecom", logo: "/sponsors/at.png" },
   { name: "CAAT", logo: "/sponsors/caat.png" },
   { name: "Cash", logo: "/sponsors/cash.svg" },
   { name: "Orbit Electronics", logo: "/sponsors/orbit.png" },
   { name: "Yalidine", logo: "/sponsors/yalidine.png" },
   { name: "Sonatrach", logo: "/sponsors/sonatrach.png" },
-  { name: "USTHB", logo: "/sponsors/usthb.png" },
+  { name: "USTHB", logo: "/sponsors/usthb-b.png" },
   { name: "Groupe Cosider", logo: "/sponsors/cosider.svg" },
   { name: "Hamoud Boualem", logo: "/sponsors/hb.png" },
   { name: "BEA", logo: "/sponsors/bea.png" },
   { name: "Saidal", logo: "/sponsors/saidal.png" },
   { name: "CNEP", logo: "/sponsors/cnep.svg" },
   { name: "CPA", logo: "/sponsors/cpa.png" },
-  { name: "HouseOfBurger", logo: "/sponsors/hob.png" },
   { name: "saa", logo: "/sponsors/saa.png" },
   { name: "huawei", logo: "/sponsors/huawei.png" },
   { name: "BK Fire", logo: "/sponsors/bk.png" },
