6abd022958e00973cc3ccbbc47d9067be969ed65
Yasser <yasserfedsi34@gmail.com>
2026-01-07 17:00:13 +0100

    feat: Add TypeScript types for events, links, and shared components

---
+++ types/index.ts
@@ -0,0 +1,15 @@
+export type eventsProps = {
+  title: string;
+  description: string;
+  image: string;
+};
+
+export type Links = {
+  name: string;
+  path: string;
+};
+
+export type SProps = {
+  name: string;
+  logo: string;
+}
