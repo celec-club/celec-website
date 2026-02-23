0880218d2d1cb8fe727e9867c490a3c6f5f347bc
Yasser <yasserfedsi34@gmail.com>
2026-02-23 01:00:07 +0100

    fix: Update event images and descriptions for national and internal events

--- src/utils/events.ts
+++ src/utils/events.ts
@@ -1,25 +1,43 @@
 import { eventsProps } from "../../types";
 
+export const internationalEvents: eventsProps[] = [
+  {
+    title: "JunctionXAlgiers",
+    description: "The Finland hackathon organized by CELEC CLUB",
+    image: "/events/international/JunctionXAlgiers.jpg",
+  },
+];
+
 export const nationalEvents: eventsProps[] = [
   {
     title: "Algerian Robot Cup",
     description: "Annual Robotics Battle",
-    image: "/events/arc-banner.jpg",
-  },
-  {
-    title: "JunctionXAlgiers",
-    description: "The Finland hackathon organized by CELEC CLUB",
-    image: "/events/JunctionXAlgiers.jpg",
+    image: "/events/national/arc-banner.jpg",
   },
   {
     title: "Django Girl X Algiers",
     description: "48-hour coding challenge",
-    image: "/events/django-girls.png",
+    image: "/events/national/dgirls.jpg",
   },
   {
     title: "C-Tech",
     description: "CELEC Technology Conference",
-    image: "/events",
+    image: "/events/national/ctech.png",
+  },
+  {
+    title: "Cyber Pink",
+    description: "",
+    image: "/events/national/cyber_pink.jpg",
+  },
+  {
+    title: "Go Network",
+    description: "",
+    image: "/events/national/go_network.png",
+  },
+  {
+    title: "Tech Race",
+    description: "",
+    image: "/events/national/tech_race.jpg",
   },
 ];
 
@@ -27,16 +45,16 @@ export const internalEvents: eventsProps[] = [
   {
     title: "Workshop Series",
     description: "Monthly skill-building sessions",
-    image: "/events/",
+    image: "/events/internal/workshops.jpg",
   },
   {
     title: "Dishack",
     description: "Discord x CELEC Hackathon",
-    image: "/events/",
+    image: "/events/internal/",
   },
   {
     title: "Gaussian Pick",
     description: "Online Discord Hackathon",
-    image: "/events/",
+    image: "/events/internal/gaussian_pick.png",
   },
 ];
